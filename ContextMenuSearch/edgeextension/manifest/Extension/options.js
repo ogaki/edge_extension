a0c6f7a6b879af762b6168d357d76313674f5751
ogaki <noritoshi.ogaki@live.jp>
2017-11-07 02:18:09 +0900

    modifying options.*

--- ContextMenuSearch/edgeextension/manifest/Extension/options.js
+++ ContextMenuSearch/edgeextension/manifest/Extension/options.js
@@ -1,9 +1,6 @@
 
 
   /*
-  Update the options UI with the settings values retrieved from storage,
-  or the default settings if the stored settings are empty.
-  */
   function updateUI(restoredSettings) {
     console.log(restoreSettings);
   }
@@ -14,3 +11,4 @@
   
   const gettingStoredSettings = window.localStorage.getItem('data');
   gettingStoredSettings.then(updateUI, onError);
+*/
