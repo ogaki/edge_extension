f6fe9b92bc461b4f2bd6a95660380bb5a4fc50d5
ogaki <noritoshi.ogaki@live.jp>
2017-11-08 04:54:18 +0900

    Enabling the search from the context menu, yet options reflected in the context menu

--- ContextMenuSearch/edgeextension/manifest/Extension/background.js
+++ ContextMenuSearch/edgeextension/manifest/Extension/background.js
@@ -1,48 +1,38 @@
-var title = browser.i18n.getMessage("title");
-var title_google = browser.i18n.getMessage("title_google");
-var title_bing = browser.i18n.getMessage("title_bing");
-var title_duckduckgo = browser.i18n.getMessage("title_duckduckgo");
+var default_options = [
+    ['google', 'https://www.google.com/search?q=%s'],
+    ['bing', 'https://www.bing.com/search?q=%s'],
+    ['duckduckgo', 'https://www.duckduckgo.com/?q=%s']];
+
 
 function onCreated() {
     if (browser.runtime.lastError) {
-        console.log(`Error: ${browser.runtime.lastError}`);
+        console.log('Error: ${browser.runtime.lastError}');
     } else {
         console.log("Item created successfully");
     }
 }
 
 //browser.runtime.onInstalled.addListener(function () {
+var data = window.localStorage.getItem('data');
+var options = default_options;
+if (data) {
+    //convert json to array
+}
+var _title;
+for (var ix = 0; ix < options.length; ix++) {
+    _title = options[ix][0];
     browser.contextMenus.create({
-        id: 'menu_search_google',
-        title: title_google,
+        id: '' + ix,
+        title: _title,
         contexts: ['selection'],
         type: "normal",
-        onclick: (info, tab) => {
-            let url =
-                'https://www.google.com/search?q=' + info.selectionText;
-            browser.tabs.create({ url: url, index: tab.index + 1 });
-        }
+        onclick: onClickContextMenu
     }, onCreated);
-    browser.contextMenus.create({
-        id: 'menu_search_bing',
-        title: title_bing,
-        contexts: ['selection'],
-        type: "normal",
-        onclick: (info, tab) => {
-            let url =
-                'https://www.bing.com/search?q=' + info.selectionText;
-            browser.tabs.create({ url: url, index: tab.index + 1 });
-        }
-    }, onCreated);
-    browser.contextMenus.create({
-        id: 'menu_search_duckduckgo',
-        title: title_duckduckgo,
-        contexts: ['selection'],
-        type: "normal",
-        onclick: (info, tab) => {
-            let url =
-                'https://duckduckgo.com/?q=' + info.selectionText;
-            browser.tabs.create({ url: url, index: tab.index + 1 });
-        }
-    }, onCreated);
-//});
+}
+
+function onClickContextMenu(info, tab) {
+    var ix = parseInt(info.menuItemId);
+    var _url = options[ix][1];
+    var __url = _url.replace('%s', info.selectionText);
+    browser.tabs.create({ url: __url, index: tab.index + 1 });
+}
--- ContextMenuSearch/edgeextension/manifest/Extension/options.js
+++ ContextMenuSearch/edgeextension/manifest/Extension/options.js
@@ -1,14 +1,55 @@
+var default_options = [
+  ['google', 'https://www.google.com/search?q=%s'],
+  ['bing', 'https://www.bing.com/search?q=%s'],
+  ['duckduckgo', 'https://www.duckduckgo.com/?q=%s']];
 
+var rowNumber = 0;
 
-  /*
-  function updateUI(restoredSettings) {
-    console.log(restoreSettings);
+function addRow(_title, _url) {
+  var tableRef = document.getElementById('table'); //.getElementsByTagName('tbody')[0];
+  console.log("rows=" + tableRef.rows.length);
+  var newRow = tableRef.insertRow(tableRef.rows.length);
+  rowNumber++;
+  if (!_title) _title = "[NAME]";
+  if (!_url) _url = "[URL ex. https://www.google.com/search?=%s]";
+  var temp =
+    '<td><input class="table-text" type="text" name="title" value="' + _title + '" /></td>'
+    + '<td><input class="table-text" type="text" name="url" value="' + _url + '" /></td>'
+    + '<td><input class="table-button" type="button" onclick="delRow(' + rowNumber
+    + ');" value="' //+ rowNumber
+    + '-" /></td>';
+  newRow.insertAdjacentHTML('afterbegin', temp);
+  newRow.setAttribute("id", "rowId=" + rowNumber);
+  //console.log("rowIndex=" + newRow.rowIndex);
+}
+function delRow(rowId) {
+  //console.log("delRow:" + rowId);
+  var tableRef = document.getElementById('table'); //.getElementsByTagName('tbody')[0];
+  var rowRef = document.getElementById('rowId=' + rowId);
+  tableRef.deleteRow(rowRef.rowIndex);
+}
+function save() {
+  var tableRef = document.getElementById('table');//.getElementsByTagName('tr')[1];
+  var json = html2json(tableRef.innerHTML);
+  console.log('***save data***');
+  console.log(JSON.stringify(json));
+  if (!!window.localStorage) {
+    window.localStorage.data = JSON.stringify(json); // Convert the object to a string.
   }
-  
-  function onError(e) {
-    console.error(e);
+}
+function load() {
+  var data;
+  if (window.localStorage && (data = JSON.parse(window.localStorage.data))) {
+    //console.log('***load data***');
+    var html = json2html(data);
+    //console.log(html);
+    var tableRef = document.getElementById('table');
+    var tbodyRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+    tableRef.removeChild(tbodyRef);
+    tableRef.insertAdjacentHTML('afterbegin', html);
+  } else {
+    for (var ix = 0; ix < default_options.length; ix++) {
+      addRow(default_options[ix][0], default_options[ix][1]);
+    }
   }
-  
-  const gettingStoredSettings = window.localStorage.getItem('data');
-  gettingStoredSettings.then(updateUI, onError);
-*/
+}
